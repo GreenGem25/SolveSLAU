de1017b9b22085ada3e133248bba566a8f45f233
GreenGem25 <chloromail@gmail.com>
2024-06-06 16:04:10 +0300

    New GUI with scrollbars

--- main.py
+++ main.py
@@ -84,14 +84,14 @@ def zeidel(A, B, eps, iter):
 
 
 # Количество переменных системы
-variables = 5
+variables = 2
 
 # Открытие основного окна
 root = Tk()
 root.title('SolveSLAU')
 root.geometry('900x480')
 root['bg'] = '#27292b'
-root.minsize(900, 480)
+root.resizable(False, False)
 icon = PhotoImage(file='calc.png')
 root.iconphoto(True, icon)
 
@@ -104,42 +104,64 @@ photo = ImageTk.PhotoImage(image)
 checkFloat = (root.register(checkFloatInput), "%P")
 checkInt = (root.register(checkIntInput), "%P")
 
+root.grid_rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+frame_main = Frame(root, bg="#27292b")
+frame_main.grid(sticky='news')
+
 #
 #   Создание полей ввода в систему
 #
 
-# Инициализация таблицы с полями ввода
-for c in range(variables*2+2): root.columnconfigure(index=c)
-for r in range(variables): root.rowconfigure(index=r)
+frame_canvas = Frame(frame_main)
+frame_canvas.grid(row=0, column=0, pady=(5, 0), sticky='nw', rowspan=variables)
+frame_canvas.grid_rowconfigure(0, weight=1)
+frame_canvas.grid_columnconfigure(0, weight=1)
+canvas = Canvas(frame_canvas, scrollregion=(0, 0, variables*2*91, variables*2*20), bg='#27292b', width=650, height=350,
+                highlightthickness=0)
+vsbX = Scrollbar(frame_canvas, orient=HORIZONTAL, command=canvas.xview)
+vsbY = Scrollbar(frame_canvas, orient=VERTICAL, command=canvas.yview)
+canvas.grid(row=0, column=0, sticky='nwes')
+vsbX.grid(row=1, column=0, sticky='ew', columnspan=variables)
+vsbY.grid(row=0, column=1, sticky='ns', rowspan=variables)
 
-# Создание таблицы с полями ввода
+frame_entries = Frame(canvas, bg='#27292b')
+canvas.create_window((0, 0), window=frame_entries, anchor='nw')
+entries = []
+
+# Добавление картинки символа системы
+symbolPicture = Label(frame_entries, image=photo, background='#27292b')
+symbolPicture.image = photo
+symbolPicture.grid(column=0, rowspan=variables, row=0, sticky='nw')
+
 for r in range(variables):
     i = 1
     for c in range(1, variables*2+2):
         if c % 2 != 0:
-            entry = Entry(validate='focus', validatecommand=checkFloat, background='#6b6b6b',
+            entry = Entry(frame_entries, validate='focus', validatecommand=checkFloat, background='#6b6b6b',
                           foreground='#fff', font=20, width=10)
+            entries.append(entry)
             entry.grid(row=r, column=c, sticky=NW, columnspan=1, rowspan=1, padx=5, pady=5)
         else:
             if i == variables:
-                label = Label(text=f'x{i} = ', background='#27292b', foreground='#fff', font=20)
+                label = Label(frame_entries, text=f'x{i} = ', background='#27292b', foreground='#fff', font=20)
                 label.grid(row=r, column=c, sticky=NW, columnspan=1, rowspan=1, ipadx=5, ipady=5)
             else:
-                label = Label(text=f'x{i} + ', background='#27292b', foreground='#fff', font=20)
+                label = Label(frame_entries, text=f'x{i} + ', background='#27292b', foreground='#fff', font=20)
                 label.grid(row=r, column=c, sticky=NW, columnspan=1, rowspan=1, ipadx=5, ipady=5)
                 i += 1
-# Добавление картинки символа системы
-symbolPicture = Label(root, image=photo, background='#27292b')
-symbolPicture.image = photo
-symbolPicture.grid(column=0, rowspan=variables, row=0)
 
 #
 #   Создание кнопок и текстовых надписей
 #
 
+frame_buttons = Frame(frame_main, bg="#27292b")
+frame_buttons.grid(row=0, column=1, sticky='news')
+
 # Надписть "Метод решения"
-methodLabel = Label(text='Метод решения:', background='#27292b', foreground='#fff', font=20)
-methodLabel.place(height=50, width=200, relx=0.73, rely=0.01)
+methodLabel = Label(frame_buttons, text='Метод решения:', background='#27292b', foreground='#fff', font=20)
+methodLabel.grid(row=0, column=2, padx=55, pady=5)
 
 # Выпадающий список для выбора методов
 methods = ['Гаусс', 'Простые итерации', 'Зейдель']
@@ -154,22 +176,23 @@ comboStyle.theme_create('combostyle', parent='alt', settings={
     }
 })
 comboStyle.theme_use('combostyle')
-combobox = Combobox(values=methods, foreground='#fff', font=20)
+combobox = Combobox(frame_buttons, values=methods, foreground='#fff', font=20)
 combobox['state'] = 'readonly'
-combobox.place(relx=0.73, rely=0.1)
+combobox.grid(row=1, column=2, pady=(5, 5))
 combobox.bind("<<ComboboxSelected>>", methodSelected)
 
 # Надписть "Количество переменных"
-varLabel = Label(text='Количество переменных:', background='#27292b', foreground='#fff', font=20)
-varLabel.place(height=50, width=200, relx=0.73, rely=0.15)
+varLabel = Label(frame_buttons, text='Количество переменных:', background='#27292b', foreground='#fff', font=20)
+varLabel.grid(row=2, column=2, pady=(5, 5))
 
 # Поле ввода для количества переменных
-varEntry = Entry(validate='focus', validatecommand=checkInt, background='#6b6b6b', foreground='#fff', font=20)
-varEntry.place(relx=0.739, rely=0.248)
+varEntry = Entry(frame_buttons, validate='focus', validatecommand=checkInt, background='#6b6b6b', foreground='#fff', font=20)
+varEntry.grid(row=3, column=2, pady=(5, 5))
 
 # Кнопка "Решить"
-btnSolve = Button(text='Решить', command=btnSolveClicked, bg='#6b6b6b',
+btnSolve = Button(frame_buttons, text='Решить', command=btnSolveClicked, bg='#6b6b6b',
                   activebackground='#27292b', fg='#fff', activeforeground='#fff', font=30)
-btnSolve.place(height=50, width=200, relx=0.73, rely=0.35)
+btnSolve.grid(row=4, column=2, pady=(15, 5))
 
+root.update_idletasks()
 root.mainloop()
